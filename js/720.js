6b3f1e5e185d17c32bc88952585bf90540952fdf
iazxq <chengang.net@gmail.com>
2012-03-11 11:07:44 +0800

    modify 720.js

---
+++ js/720.js
@@ -0,0 +1,14 @@
+if (parent.location.href.indexOf("sharejs.com")>0)
+{
+document.writeln("<script type=\"text\/javascript\"><!--");
+document.writeln("google_ad_client = \"pub-0652501482160045\";");
+document.writeln("\/* 728x90, ´´½¨ÓÚ 08-12-25 *\/");
+document.writeln("google_ad_slot = \"3889147664\";");
+document.writeln("google_ad_width = 728;");
+document.writeln("google_ad_height = 90;");
+document.writeln("\/\/-->");
+document.writeln("<\/script>");
+document.writeln("<script type=\"text\/javascript\"");
+document.writeln("src=\"http:\/\/pagead2.googlesyndication.com\/pagead\/show_ads.js\">");
+document.writeln("<\/script>");
+}
